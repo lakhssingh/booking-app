5e877e081b24f12e5874a945b040b2b9ae8cb03e
Lakhs <lakhbir.shah@yahoo.co.uk>
2024-10-11 17:06:21 +0100

    added booking page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
+import Heading from "@/components/Heading";
 import RoomCard from "@/components/RoomCard";
 import rooms from "@/data/rooms.json";
 
 export default function Home() {
   return (
     <>
+      <Heading title="Avaliable Rooms" />
       {rooms.length > 0 ? (
         rooms.map((room) => <RoomCard room={room} />)
       ) : (
