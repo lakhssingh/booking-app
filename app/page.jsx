478c6fea2f7bd51e8f3bd344439c4eeb03dd67cd
Lakhs <lakhbir.shah@yahoo.co.uk>
2024-10-11 14:34:15 +0100

    RoomCard crfeated

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,14 @@
+import RoomCard from "@/components/RoomCard";
+import rooms from "@/data/rooms.json";
+
 export default function Home() {
-  return <h1>Bookit app</h1>;
+  return (
+    <>
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} />)
+      ) : (
+        <p>No rooms avaliable at the moment</p>
+      )}
+    </>
+  );
 }
